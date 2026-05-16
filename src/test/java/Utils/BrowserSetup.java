7efb5828e97176565127ac594caaee81a567f8dc
Melsa87 <thobejane.melsa@gmail.com>
2026-05-16 11:53:21 +0200

    modified BrowserSetup and Base

--- src/test/java/Utils/BrowserSetup.java
+++ src/test/java/Utils/BrowserSetup.java
@@ -1,4 +1,29 @@
 package Utils;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+
 public class BrowserSetup {
+    static WebDriver driver;
+
+    public static WebDriver startBrowser(String browser, String url) {
+        ChromeOptions chromeoptions = new ChromeOptions();
+        if (browser.equalsIgnoreCase("chrome")) {
+            //chromeoptions.addArguments("--remote-allow-origins=*");
+            driver = new ChromeDriver(chromeoptions);
+        } else if (browser.equalsIgnoreCase("edge")) {
+            driver = new EdgeDriver();
+        }  else {
+            throw new IllegalArgumentException("Unsupported browser: " + browser);
+
+        }
+        driver.manage().window().maximize();
+        driver.get(url);
+        return driver;
+
+    }
+}
+
 }
